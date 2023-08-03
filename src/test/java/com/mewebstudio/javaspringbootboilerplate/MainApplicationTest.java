1c117599c2349ad68298923241625d9f1f5d5df9
Muharrem ERİN <erin.muharrem@gmail.com>
2023-08-03 21:56:23 +0300

    MainApplication context test

---
+++ src/test/java/com/mewebstudio/javaspringbootboilerplate/MainApplicationTest.java
@@ -0,0 +1,16 @@
+package com.mewebstudio.javaspringbootboilerplate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Tag("context")
+@SpringBootTest
+@DisplayName("Context test for MainApplication")
+class MainApplicationTest {
+    @Test
+    public void applicationContextLoadedTest() {
+        MainApplication.main(new String[] {});
+    }
+}
