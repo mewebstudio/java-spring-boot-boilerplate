6286bb8bb2943adf6e58b87d4ed29e43769f8187
Muharrem ERİN <erin.muharrem@gmail.com>
2023-08-03 22:11:03 +0300

    MainApplication context test

--- src/test/java/com/mewebstudio/javaspringbootboilerplate/MainApplicationTest.java
+++ src/test/java/com/mewebstudio/javaspringbootboilerplate/MainApplicationTest.java
@@ -3,14 +3,31 @@ package com.mewebstudio.javaspringbootboilerplate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 
 @Tag("context")
 @SpringBootTest
 @DisplayName("Context test for MainApplication")
 class MainApplicationTest {
     @Test
+    @DisplayName("Context loads successfully")
     public void applicationContextLoadedTest() {
-        MainApplication.main(new String[] {});
+        try (ConfigurableApplicationContext context = mock(ConfigurableApplicationContext.class)) {
+            // Given
+            MockedStatic<SpringApplication> utilities = mockStatic(SpringApplication.class);
+            utilities.when((MockedStatic.Verification) SpringApplication.run(MainApplication.class, new String[]{}))
+                .thenReturn(null);
+            // When
+            MainApplication.main(new String[]{});
+            // Then
+            assertThat(SpringApplication.run(MainApplication.class)).isEqualTo(null);
+        }
     }
 }
